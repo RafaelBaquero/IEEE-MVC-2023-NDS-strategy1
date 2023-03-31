1d0db8bfe2be07ddcce32fd1b999a52537fe174f
RafaelBaquero <rafaelbaquero97@gmail.com>
2023-03-31 15:51:26 +0200

    trial 1

--- FMI_Init.m
+++ FMI_Init.m
@@ -1,7 +1,15 @@
 %% Initialize FMI Kit
 % download and extract the distribution archive to the current folder
-unzip(['https://github.com/CATIA-Systems/FMIKit-Simulink/releases/' ...
-  'download/v3.0/FMIKit-Simulink-3.0.zip'], 'FMIKit-Simulink-3.0')
+% Test here
+
+file = fullfile(cd, 'FMIKit-Simulink-3.0.zip');
+urlwrite(['https://github.com/CATIA-Systems/FMIKit-Simulink/releases/' ...
+  'download/v3.0/FMIKit-Simulink-3.0.zip'], ...
+          file);
+unzip(file,'FMIKit-Simulink-3.0');
+
+% unzip(['https://github.com/CATIA-Systems/FMIKit-Simulink/releases/' ...
+%   'download/v3.0/FMIKit-Simulink-3.0.zip'], 'FMIKit-Simulink-3.0')
 
 % add the folder to the MATLAB path
 addpath(fullfile(pwd, 'FMIKit-Simulink-3.0'))
